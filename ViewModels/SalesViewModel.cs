a1dc25f6f935d4e5a22040ee08983c849aaa031e
Simon Eiderbrant <simon.eiderbrant@wsp.com>
2021-08-05 11:54:28 +0200

    Added first specflow feature and viewmodel. Not running yet

---
+++ ViewModels/SalesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace loppis.ViewModels
+{
+    public class SalesViewModel
+    {
+        public SalesViewModel()
+        {
+            ItemList = new List<Tuple<int, int>>();
+        }
+
+        public int SellerId { get; set; }
+        public int Price { get; set; }
+        public int Sum { get; set; }
+        public List<Tuple<int, int>> ItemList { get; set; }
+
+        public void EnterSale() { }
+    }
+}
