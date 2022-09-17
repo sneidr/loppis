040c41756b4aa9fccc869fa89f18dcaa6f7e804f
Simon <eiderbrant@gmail.com>
2022-09-17 11:59:05 +0200

    MoveFocus tests moved to own file Tests divided and renamed

--- LoppisTest/LoppisTest.cs
+++ LoppisTest/LoppisTest.cs
@@ -11,34 +11,6 @@ namespace LoppisTest;
 [TestClass]
 public class LoppisTest
 {
-    #region Move Command Tests
-
-    [TestMethod]
-    public void TestMove_InvalidSellerId()
-    {
-        TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
-
-        SalesViewModel vm = new();
-        Assert.AreEqual(Colors.White, ((SolidColorBrush)vm.SellerIdBackground).Color);
-
-        Assert.IsTrue(vm.LoadCommand.CanExecute(null));
-        vm.LoadCommand.Execute(null);
-
-        vm.CurrentEntry.Price = 62;
-        vm.CurrentEntry.SellerId = 12;
-        Assert.IsFalse(vm.MoveFocusCommand.CanExecute(null));
-        Assert.AreEqual(Colors.Orange, ((SolidColorBrush)vm.SellerIdBackground).Color);
-
-        vm.CurrentEntry.SellerId = 7;
-        Assert.IsTrue(vm.MoveFocusCommand.CanExecute(null));
-        Assert.AreEqual(Colors.White, ((SolidColorBrush)vm.SellerIdBackground).Color);
-
-        vm.CurrentEntry.SellerId = 999;
-        Assert.IsTrue(vm.MoveFocusCommand.CanExecute(null));
-    }
-
-    #endregion
-
     #region Bag Command Tests
     [TestMethod]
     public void TestBagCommand_CanExecute()
---
+++ LoppisTest/MoveFocus.cs
@@ -0,0 +1,72 @@
+﻿using loppis.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Media;
+
+namespace LoppisTest;
+
+[TestClass]
+public class MoveFocus
+{
+    [TestMethod]
+    public void Can_Move_Focus_When_SellerId_Is_Valid()
+    {
+        TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
+
+        SalesViewModel vm = new();
+        vm.LoadCommand.Execute(null);
+
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 7;
+        Assert.IsTrue(vm.MoveFocusCommand.CanExecute(null));
+    }
+
+    [TestMethod]
+    public void Cannot_Move_Focus_When_SellerId_Is_Invalid()
+    {
+        TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
+
+        SalesViewModel vm = new();
+        vm.LoadCommand.Execute(null);
+
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 12;
+        Assert.IsFalse(vm.MoveFocusCommand.CanExecute(null));
+    }
+
+    [TestMethod]
+    public void SellerId_Background_Is_White_When_SellerId_Is_Valid()
+    {
+        TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
+
+        SalesViewModel vm = new();
+        vm.LoadCommand.Execute(null);
+
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 7;
+
+        vm.MoveFocusCommand.CanExecute(null);
+        Assert.AreEqual(Colors.White, ((SolidColorBrush)vm.SellerIdBackground).Color);
+    }
+
+    [TestMethod]
+    public void SellerId_Background_Is_Orange_When_SellerId_Is_Invalid()
+    {
+        TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
+
+        SalesViewModel vm = new();
+        vm.LoadCommand.Execute(null);
+
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 12;
+        
+        vm.MoveFocusCommand.CanExecute(null);
+        Assert.AreEqual(Colors.Orange, ((SolidColorBrush)vm.SellerIdBackground).Color);
+    }
+
+    [TestMethod]
+    public void SellerId_Background_Is_White_Before_Any_Move()
+    {
+        SalesViewModel vm = new();
+        Assert.AreEqual(Colors.White, ((SolidColorBrush)vm.SellerIdBackground).Color);
+    }
+}
