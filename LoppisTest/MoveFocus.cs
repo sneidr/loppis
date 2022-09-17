7b014fd9c802c7530cd7a947c377a0e54fc591a3
Simon <eiderbrant@gmail.com>
2022-09-17 12:06:06 +0200

    Added datadriven tests

--- LoppisTest/MoveFocus.cs
+++ LoppisTest/MoveFocus.cs
@@ -7,8 +7,14 @@ namespace LoppisTest;
 [TestClass]
 public class MoveFocus
 {
-    [TestMethod]
-    public void Can_Move_Focus_When_SellerId_Is_Valid()
+    [DataTestMethod]
+    [DataRow(1)]
+    [DataRow(2)]
+    [DataRow(7)]
+    [DataRow(8)]
+    [DataRow(9)]
+    [DataRow(999)] // 999 is RoundUp default value
+    public void Can_Move_Focus_When_SellerId_Is_Valid(int sellerId)
     {
         TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
 
@@ -16,12 +22,18 @@ public class MoveFocus
         vm.LoadCommand.Execute(null);
 
         vm.CurrentEntry.Price = 62;
-        vm.CurrentEntry.SellerId = 7;
+        vm.CurrentEntry.SellerId = sellerId;
         Assert.IsTrue(vm.MoveFocusCommand.CanExecute(null));
     }
 
-    [TestMethod]
-    public void Cannot_Move_Focus_When_SellerId_Is_Invalid()
+    [DataTestMethod]
+    [DataRow(0)]
+    [DataRow(-1)]
+    [DataRow(5)]
+    [DataRow(100)]
+    [DataRow(int.MaxValue)]
+    [DataRow(int.MinValue)]
+    public void Cannot_Move_Focus_When_SellerId_Is_Invalid(int sellerId)
     {
         TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
 
@@ -29,7 +41,7 @@ public class MoveFocus
         vm.LoadCommand.Execute(null);
 
         vm.CurrentEntry.Price = 62;
-        vm.CurrentEntry.SellerId = 12;
+        vm.CurrentEntry.SellerId = sellerId;
         Assert.IsFalse(vm.MoveFocusCommand.CanExecute(null));
     }
 
