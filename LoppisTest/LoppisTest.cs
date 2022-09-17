8305821fb1a69af02379f09880b54b87f20d2f1b
Simon <eiderbrant@gmail.com>
2022-09-17 11:19:35 +0200

    Moved test file setup class to its own file

--- LoppisTest/LoppisTest.cs
+++ LoppisTest/LoppisTest.cs
@@ -8,59 +8,6 @@ using SaveList = System.Collections.Generic.List<loppis.Model.Sale>;
 
 namespace LoppisTest;
 
-internal class TestFiles
-{
-    private const string sellerFile = @".\sellers.csv";
-    public static string TransactionsFile => @".\mytestfile.xml";
-
-    public static string FirstErrorFile = $"{Path.GetFileNameWithoutExtension(TestFiles.TransactionsFile)}_error1{Path.GetExtension(TestFiles.TransactionsFile)}";
-    public static string SeccondErrorFile = $"{Path.GetFileNameWithoutExtension(TestFiles.TransactionsFile)}_error2{Path.GetExtension(TestFiles.TransactionsFile)}";
-
-    public static void RemoveTransactionsFile()
-    {
-        if (File.Exists(TransactionsFile))
-        {
-            File.Delete(TransactionsFile);
-        }
-    }
-
-    public static void SetupTransactionsFile(string contents)
-    {
-        RemoveTransactionsFile();
-
-        File.Create(TransactionsFile).Close();
-        File.WriteAllText(TransactionsFile, contents);
-    }
-
-    public static void RemoveConfigFile()
-    {
-        if (File.Exists(sellerFile))
-        {
-            File.Delete(sellerFile);
-        }
-    }
-
-    public static void SetupConfigFile(string contents)
-    {
-        RemoveConfigFile();
-
-        File.Create(sellerFile).Close();
-        File.WriteAllText(sellerFile, contents);
-    }
-
-    public static void RemoveErrorFiles()
-    {
-        if (File.Exists(FirstErrorFile))
-        {
-            File.Delete(FirstErrorFile);
-        }
-        if (File.Exists(SeccondErrorFile))
-        {
-            File.Delete(SeccondErrorFile);
-        }
-    }
-}
-
 [TestClass]
 public class LoppisTest
 {
