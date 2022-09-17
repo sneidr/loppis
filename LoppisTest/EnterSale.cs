c171382c954a1ea9350dfceca6dc10a47d744af5
Simon <eiderbrant@gmail.com>
2022-09-17 11:42:10 +0200

    Moved EnterSale tests to their own class(es) and file Renamed tests

---
+++ LoppisTest/EnterSale.cs
@@ -0,0 +1,157 @@
+﻿using loppis.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Media;
+
+namespace LoppisTest;
+
+[TestClass]
+public class EnterSale
+{
+    #region EnterSale Tests
+
+    [TestMethod]
+    public void Sum_Is_Equal_To_Price_When_Entering_One_Sale()
+    {
+        SalesViewModel vm = new();
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 12;
+        vm.EnterSale();
+
+        Assert.AreEqual(vm.SumTotal, 62);
+    }
+
+    [TestMethod]
+    public void List_Has_One_Entry_When_Entering_One_Sale()
+    {
+        SalesViewModel vm = new();
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 12;
+        vm.EnterSale();
+
+        Assert.AreEqual(vm.ItemList.Count, 1);
+    }
+
+    [TestMethod]
+    public void List_Entry_Is_Correct_When_Entering_One_Sale()
+    {
+        SalesViewModel vm = new();
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 12;
+        vm.EnterSale();
+
+        Assert.AreEqual(vm.ItemList[0].SellerId, 12);
+        Assert.AreEqual(vm.ItemList[0].SellerIdListText, "Säljare:  12");
+        Assert.AreEqual(vm.ItemList[0].Price, 62);
+    }
+
+    [TestMethod]
+    public void All_Sales_Are_Added_To_List_When_Entering_Multiple_Sales()
+    {
+        SalesViewModel vm = new();
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 12;
+        vm.EnterSale();
+        vm.CurrentEntry.Price = 55;
+        vm.CurrentEntry.SellerId = 15;
+        vm.EnterSale();
+
+        Assert.AreEqual(vm.ItemList[0].Price, 55);
+        Assert.AreEqual(vm.ItemList[1].Price, 62);
+    }
+
+    [TestMethod]
+    public void Sum_Is_Total_Of_Sales_When_Entering_Multiple_Sales()
+    {
+        SalesViewModel vm = new();
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 12;
+        vm.EnterSale();
+        vm.CurrentEntry.Price = 55;
+        vm.CurrentEntry.SellerId = 15;
+        vm.EnterSale();
+
+        Assert.AreEqual(vm.SumTotal, 62 + 55);
+    }
+
+    [TestMethod]
+    public void Current_Entry_Is_Cleared_When_Entering_Sale()
+    {
+        SalesViewModel vm = new();
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 12;
+        vm.EnterSale();
+
+        Assert.IsNull(vm.CurrentEntry.Price);
+        Assert.IsNull(vm.CurrentEntry.SellerId);
+    }
+    #endregion
+}
+
+[TestClass]
+public class CanEnterSale
+{
+    [TestMethod]
+    public void Can_Enter_Sale_When_SellerId_Is_Valid()
+    {
+        TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
+
+        SalesViewModel vm = new();
+        vm.LoadCommand.Execute(null);
+
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 7;
+        Assert.IsTrue(vm.EnterSaleCommand.CanExecute(null));
+    }
+
+    [TestMethod]
+    public void Cannot_Enter_Sale_When_SellerId_Is_Invalid()
+    {
+        TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
+
+        SalesViewModel vm = new();
+        vm.LoadCommand.Execute(null);
+
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 12;
+        Assert.IsFalse(vm.EnterSaleCommand.CanExecute(null));
+    }
+
+    [TestMethod]
+    public void SellerId_Background_White_When_SellerId_Is_Valid()
+    {
+        TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
+
+        SalesViewModel vm = new();
+        vm.LoadCommand.Execute(null);
+
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 7;
+        Assert.AreEqual(Colors.White, ((SolidColorBrush)vm.SellerIdBackground).Color);
+    }
+
+    [TestMethod]
+    public void SellerId_Background_Orange_When_SellerId_Is_Invalid()
+    {
+        TestFiles.SetupConfigFile("1;Firstname LastName\r\n2;John Doe\r\n7;Hej Svej\r\n8;Kasse;1\r\n9;Vykort;2");
+
+        SalesViewModel vm = new();
+        Assert.AreEqual(Colors.White, ((SolidColorBrush)vm.SellerIdBackground).Color);
+        vm.LoadCommand.Execute(null);
+
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 12;
+        Assert.IsFalse(vm.EnterSaleCommand.CanExecute(null));
+        Assert.AreEqual(Colors.Orange, ((SolidColorBrush)vm.SellerIdBackground).Color);
+    }
+
+    [TestMethod]
+    public void Can_Enter_Sale_When_SellerId_Is_999_For_RoundUp()
+    {
+        SalesViewModel vm = new();
+        vm.CurrentEntry.Price = 62;
+        vm.CurrentEntry.SellerId = 999;
+        Assert.IsTrue(vm.EnterSaleCommand.CanExecute(null));
+    }
+
+}
+
---
+++ LoppisTest/TestFiles.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+
+namespace LoppisTest;
+
+internal class TestFiles
+{
+    private const string sellerFile = @".\sellers.csv";
+    public static string TransactionsFile => @".\mytestfile.xml";
+
+    public static string FirstErrorFile = $"{Path.GetFileNameWithoutExtension(TestFiles.TransactionsFile)}_error1{Path.GetExtension(TestFiles.TransactionsFile)}";
+    public static string SeccondErrorFile = $"{Path.GetFileNameWithoutExtension(TestFiles.TransactionsFile)}_error2{Path.GetExtension(TestFiles.TransactionsFile)}";
+
+    public static void RemoveTransactionsFile()
+    {
+        if (File.Exists(TransactionsFile))
+        {
+            File.Delete(TransactionsFile);
+        }
+    }
+
+    public static void SetupTransactionsFile(string contents)
+    {
+        RemoveTransactionsFile();
+
+        File.Create(TransactionsFile).Close();
+        File.WriteAllText(TransactionsFile, contents);
+    }
+
+    public static void RemoveConfigFile()
+    {
+        if (File.Exists(sellerFile))
+        {
+            File.Delete(sellerFile);
+        }
+    }
+
+    public static void SetupConfigFile(string contents)
+    {
+        RemoveConfigFile();
+
+        File.Create(sellerFile).Close();
+        File.WriteAllText(sellerFile, contents);
+    }
+
+    public static void RemoveErrorFiles()
+    {
+        if (File.Exists(FirstErrorFile))
+        {
+            File.Delete(FirstErrorFile);
+        }
+        if (File.Exists(SeccondErrorFile))
+        {
+            File.Delete(SeccondErrorFile);
+        }
+    }
+}
