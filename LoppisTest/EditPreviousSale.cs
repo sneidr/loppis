5b969b5f28666c409f51dc5c884a21fd9c44292c
Simon <eiderbrant@gmail.com>
2022-10-02 10:46:23 +0200

    Updated CanEditPreviousSale tests

--- LoppisTest/EditPreviousSale.cs
+++ LoppisTest/EditPreviousSale.cs
@@ -1,5 +1,6 @@
 ﻿using loppis.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 using SaveList = System.Collections.Generic.List<loppis.Model.Sale>;
@@ -9,20 +10,18 @@ namespace LoppisTest;
 [TestClass]
 public class EditPreviousSale
 {
-    public void SetupSellerList(SalesViewModel vm)
-    {
-        vm.SellerList.Add(3, new Seller() { Name = "Lisa" });
-        vm.SellerList.Add(5, new Seller() { Name = "Humle Dumle" });
-        vm.SellerList.Add(7, new Seller() { Name = "Pippi" });
-    }
+    public static Dictionary<int, Seller> TestSellerList => new()
+        {
+            { 3, new Seller() { Name = "Lisa" } },
+            { 5, new Seller() { Name = "Humle Dumle" } },
+            { 7, new Seller() { Name = "Pippi" } }
+        };
 
     [TestMethod]
     public void Previous_Item_Should_End_Up_In_List_When_Doing_Edit_Previous()
     {
         TestFiles.RemoveTransactionsFile();
-        SalesViewModel vm = new(TestFiles.TransactionsFile) { Cashier = "Simon"};
-        SetupSellerList(vm);
-
+        SalesViewModel vm = new(TestFiles.TransactionsFile) { Cashier = "Simon", SellerList = TestSellerList };
         vm.CurrentEntry.SellerId = 3;
         vm.CurrentEntry.Price = 34;
 
@@ -34,44 +33,14 @@ public class EditPreviousSale
         Assert.AreEqual(vm.ItemList.Count, 1);
         Assert.AreEqual(vm.ItemList[0].Price, 34);
         Assert.AreEqual(vm.ItemList[0].SellerId, 3);
+        Assert.AreEqual(34, vm.SumTotal);
     }
-
-    //[TestMethod]
-    //public void Previous_Item_Should_End_Up_In_List_When_Doing_Edit_Previous()
-    //{
-    //    TestFiles.RemoveTransactionsFile();
-    //    SalesViewModel vm = new(TestFiles.TransactionsFile);
-
-    //    vm.CurrentEntry.SellerId = 3;
-    //    vm.CurrentEntry.Price = 34;
-
-    //    vm.SellerList.Add(3, new Seller() { Name = "Lisa" });
-    //    vm.SellerList.Add(5, new Seller() { Name = "Humle Dumle" });
-
-    //    vm.Cashier = "Simon";
-
-    //    vm.EnterSaleCommand.Execute(null);
-    //    vm.SaveToFileCommand.Execute(null);
-
-    //    vm.CurrentEntry.SellerId = 5;
-    //    vm.CurrentEntry.Price = 54;
-
-    //    vm.EnterSaleCommand.Execute(null);
-    //    vm.SaveToFileCommand.Execute(null);
-
-    //    vm.EditPreviousSaleCommand.Execute(1);
-
-    //    Assert.AreEqual(vm.ItemList.Count, 1);
-    //    Assert.AreEqual(vm.ItemList[0].Price, 34);
-    //    Assert.AreEqual(vm.ItemList[0].SellerId, 3);
-    //}
 
     [TestMethod]
     public void Previous_Item_Should_Be_Removed_From_Last_Sales_When_Doing_Edit_Previous()
     {
         TestFiles.RemoveTransactionsFile();
-        SalesViewModel vm = new(TestFiles.TransactionsFile) { Cashier = "Simon" };
-        SetupSellerList(vm);
+        SalesViewModel vm = new(TestFiles.TransactionsFile) { Cashier = "Simon", SellerList = TestSellerList };
 
         vm.CurrentEntry.SellerId = 3;
         vm.CurrentEntry.Price = 34;
@@ -81,16 +50,37 @@ public class EditPreviousSale
 
         vm.EditPreviousSaleCommand.Execute(0);
 
-        Assert.AreEqual(34, vm.SumTotal);
         Assert.AreEqual(0, vm.LastSalesList.Count);
+    }
+
+    [TestMethod]
+    public void Previous_Item_Should_Be_Removed_From_Transactions_File_When_Doing_Edit_Previous()
+    {
+        TestFiles.RemoveTransactionsFile();
+        SalesViewModel vm = new(TestFiles.TransactionsFile) { Cashier = "Simon", SellerList = TestSellerList };
+
+        vm.CurrentEntry.SellerId = 3;
+        vm.CurrentEntry.Price = 34;
+
+        vm.EnterSaleCommand.Execute(null);
+        vm.SaveToFileCommand.Execute(null);
+
+        vm.EditPreviousSaleCommand.Execute(0);
+
+        var entries = new SaveList();
+        using (var filestream = new FileStream(TestFiles.TransactionsFile, FileMode.Open))
+        {
+            var xmlreader = new XmlSerializer(typeof(SaveList));
+            entries = (SaveList)xmlreader.Deserialize(filestream);
+        }
+        Assert.AreEqual(entries.Count, 0);
     }
 
     [TestMethod]
     public void TestEditPreviousSaleCommand()
     {
         TestFiles.RemoveTransactionsFile();
-        SalesViewModel vm = new(TestFiles.TransactionsFile) { Cashier = "Simon" };
-        SetupSellerList(vm);
+        SalesViewModel vm = new(TestFiles.TransactionsFile) { Cashier = "Simon", SellerList = TestSellerList };
 
         vm.CurrentEntry.SellerId = 3;
         vm.CurrentEntry.Price = 34;
@@ -151,6 +141,12 @@ public class EditPreviousSale
 [TestClass]
 public class CanEditPreviousSale
 {
+    public static Dictionary<int, Seller> TestSellerList => new()
+        {
+            { 3, new Seller() { Name = "Lisa" } },
+            { 5, new Seller() { Name = "Humle Dumle" } }
+        };
+
     [TestMethod]
     public void Cannot_Edit_Previous_Sale_When_No_Sale_Has_Been_Entered()
     {
@@ -165,9 +161,7 @@ public class CanEditPreviousSale
     {
         TestFiles.RemoveTransactionsFile();
 
-        SalesViewModel vm = new(TestFiles.TransactionsFile);
-        vm.Cashier = "Simon";
-        vm.SellerList.Add(3, new Seller() { Name = "Lisa" });
+        SalesViewModel vm = new(TestFiles.TransactionsFile) { Cashier = "Simon", SellerList = TestSellerList };
 
         vm.CurrentEntry.SellerId = 3;
         vm.CurrentEntry.Price = 34;
@@ -181,16 +175,39 @@ public class CanEditPreviousSale
     public void Cannot_Edit_Previous_Sale_When_Already_Editing()
     {
         TestFiles.RemoveTransactionsFile();
-
-        SalesViewModel vm = new(TestFiles.TransactionsFile);
-        vm.Cashier = "Simon";
-        vm.SellerList.Add(3, new Seller() { Name = "Lisa" });
+        SalesViewModel vm = new(TestFiles.TransactionsFile) { Cashier = "Simon", SellerList = TestSellerList };
 
         vm.CurrentEntry.SellerId = 3;
         vm.CurrentEntry.Price = 34;
 
         vm.EnterSaleCommand.Execute(null);
         vm.SaveToFileCommand.Execute(null);
+
+        vm.EditPreviousSaleCommand.Execute(0);
+        Assert.IsFalse(vm.EditPreviousSaleCommand.CanExecute(null));
+    }
+
+    [TestMethod]
+    public void Can_Edit_Previous_Sale_After_Clearing_Edit()
+    {
+        TestFiles.RemoveTransactionsFile();
+        SalesViewModel vm = new(TestFiles.TransactionsFile) { Cashier = "Simon", SellerList = TestSellerList };
+
+        vm.CurrentEntry.SellerId = 3;
+        vm.CurrentEntry.Price = 34;
+
+        vm.EnterSaleCommand.Execute(null);
+        vm.SaveToFileCommand.Execute(null);
+
+        vm.CurrentEntry.SellerId = 5;
+        vm.CurrentEntry.Price = 44;
+
+        vm.EnterSaleCommand.Execute(null);
+        vm.SaveToFileCommand.Execute(null);
+
+        vm.EditPreviousSaleCommand.Execute(0);
+
+        vm.UndoCommand.Execute(0);
         Assert.IsTrue(vm.EditPreviousSaleCommand.CanExecute(null));
     }
 }
